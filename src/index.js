34b2a2cddf175f9404f0ab8717b4ba5a92e4be88
RZFalcore <box.rz.box@gmail.com>
2020-05-25 15:27:42 +0300

    promices

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import './styles.css';
-import moduleTemaplete from './js/10-Templates';
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const success = Math.random() < 0.3;
+    if (success) {
+      resolve('Promice resolved.');
+    } else {
+      reject('Promice rejected.');
+    }
+  }, 500);
+});
+
+// Var_1
+// promise.then(
+//   message => {
+//     console.log(message);
+//   },
+//   error => {
+//     console.error(`Error - ${error}`);
+//   },
+// );
+
+// Var_2
+// const onSuccess = message => {
+//   console.log(message);
+// };
+// const onError = error => {
+//   console.error(`Error - ${error}`);
+// };
+
+// promise.then(onSuccess, onError);
+
+// Var_3
+const onSuccess = message => {
+  console.log(message);
+};
+const onError = error => {
+  console.error(`Error - ${error}`);
+};
+
+promise.then(onSuccess).catch(onError);

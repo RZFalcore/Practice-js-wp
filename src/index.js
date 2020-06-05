95bac2aa67c43019cf24318077073a4da520fac6
RZFalcore <box.rz.box@gmail.com>
2020-06-06 02:32:46 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-import './styles.css';
-import moduleTemaplete from './js/10-Templates';
+import tableTmp from './templates/table.hbs';
+const tableBody = document.querySelector('#planets');
+
+const baseUrl = 'https://swapi.dev/api/';
+
+const options = {
+  Accept: 'Application/json',
+};
+
+fetch(baseUrl + 'planets', options)
+  .then(resp => {
+    return resp.json();
+  })
+  .then(data => {
+    renderTableRows(data.results);
+  });
+
+function renderTableRows(planets) {
+  const markup = planets.map(planet => tableTmp(planet)).join('');
+  tableBody.insertAdjacentHTML('beforeend', markup);
+}
